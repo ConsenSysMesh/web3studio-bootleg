2e8604b987a6713bcf161b21ec1438f96b6f8b07
Brian Chamberlain <brian.chamberlain@consensys.net>
2019-02-20 10:10:13 -0500

    Updated from code review

--- packages/bootleg-tokens/contracts/ISharedRoyaltyTokenEnumerable.sol
+++ packages/bootleg-tokens/contracts/ISharedRoyaltyTokenEnumerable.sol
@@ -6,26 +6,32 @@ pragma solidity ^0.5.3;
 contract ISharedRoyaltyTokenEnumerable {
 
   /**
-  * @notice Gets the total number of Franchisees given a Token ID
+  * @notice Gets the total number of franchisors given a Token ID
   * @param _tokenId The identifier of the NFT
-  * @return uint256 The number of franchisees
+  * @return uint256 The number of franchisors
   */
-  function tokenFranchiseesTotal(uint256 _tokenId) public view returns (uint256 count);
+  function tokenFranchisorsTotal(uint256 _tokenId) public view returns (uint256 count);
 
   /**
-  * @notice Gets the token franchisee for a given token at a specific index
+  * @notice Gets the token franchisor for a given token at a specific index
   * @param _tokenId The identifier of the NFT
-  * @param _index The index, should be less than total franchisees for the token
-  * @return address The address of the franchisee
+  * @param _index The index (should be less than total franchisors for the token)
+  * @return address The address of the franchisor
   */
-  function tokenFranchiseesByIndex(uint256 _tokenId, uint256 _index) public view returns (address owner);
+  function tokenFranchisorsByIndex(uint256 _index, uint256 _tokenId) public view returns (address owner);
 
   /**
-  * @notice Utility function gets the payout in eth for the token and franchisee at a given index for a given amount.
-  * @dev Implement the payment model here based on your distribution design (e.g. bookend, linear, weighted curve, etc..)
+  * @notice Gets the total number of payments given a Token ID
   * @param _tokenId The identifier of the NFT
-  * @param _index The index, should be less than total franchisees for the token
-  * @param _amount The ammount, in eth, total sale price for the token
+  * @return uint256 The number of payments
   */
-  function calculatePayoutOfFranchiseeByIndex(uint256 _tokenId, uint256 _index, uint256 _amount) public view returns (uint256 payout);
+  function tokenPaymentsTotal(uint256 _tokenId) public view returns (uint256 count);
+
+  /**
+  * @notice Gets the token payment for a given token at a specific index
+  * @param _tokenId The identifier of the NFT
+  * @param _index The index (should be less than total payments for the token)
+  * @return unint256 The payment amount
+  */
+  function tokenPaymentsByIndex(uint256 _index, uint256 _tokenId) public view returns (uint256 amount);
 }
