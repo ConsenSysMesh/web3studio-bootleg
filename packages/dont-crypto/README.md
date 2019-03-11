d1e6438f4e2e14f2743be5b2f5f373db486df605
Hadas Zeilberger <hadaszeilberger@gmail.com>
2019-03-10 21:06:22 -0400

    added support for dynamodb, wrote some setup instructions

--- packages/dont-crypto/README.md
+++ packages/dont-crypto/README.md
@@ -1,3 +1,14 @@
 # dont-crypto
 
 This is the directory where the dont cryptography implementation will live
+
+To use:
+In main directory do:
+`npm install`
+`stack build`
+
+To test, do
+`serverless offline start`
+
+http://localhost:3000/getintarray will show an int array (in next commit, it will be retrieving a value from DynamoDB. )
+http://localhost:3000/getrejected will show {"message":"unauthorized"} because requests go through a not-yet-working authorization function
--- packages/dont-crypto/app/Main.hs
+++ packages/dont-crypto/app/Main.hs
@@ -1,14 +1,55 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+import DontDynamoDB
+import Network.AWS.Easy
 import AWSLambda.Events.APIGateway
+import Data.ByteString
 import Control.Lens
+import Control.Exception.Lens (handling)
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.AWS
+import Data.Monoid ((<>))
+import Data.Text (Text, pack)
+import qualified Data.Text.IO as Text
+import qualified Data.Text.Encoding as Encoding
+import qualified Data.Text as Text
 import Data.Aeson
 import Data.Aeson.Embedded
+import Network.AWS.DynamoDB
+import System.IO
+import Network.AWS.Data
+import Data.HashMap.Strict     (HashMap)
+import qualified Data.HashMap.Strict     as Map
+import Network.AWS.DynamoDB.Types
+import Network.AWS.SQS 
+    (
+        _QueueDoesNotExist
+      , createQueue
+      , getQueueURL
+      , gqursQueueURL
+      , listQueues
+      , lqrsQueueURLs
+      , mBody
+      , receiveMessage
+      , rmrsMessages
+      , sendMessage
+      , SendMessageResponse
+      , sqs
+    )
+    
+
 main = apiGatewayMain handler
 
+
+newtype QueueURL = QueueURL Text deriving Show
+
+--function that gets queue url
 handler :: APIGatewayProxyRequest (Embedded Value) -> IO (APIGatewayProxyResponse (Embedded [Int]))
 handler request = do
-  putStrLn "This should go to logs"
   print $ request ^. requestBody
+  insertItem Ohio False (Encoding.encodeUtf8 (Data.Text.pack "hostname")) 80 (Data.Text.pack "Dont") (Map.fromList [((Data.Text.pack "id"), (attributeValue & avS .~ Just (Text.pack "hello")))])  
   pure $ responseOK & responseBodyEmbedded ?~ [1,2,3]
 
 
