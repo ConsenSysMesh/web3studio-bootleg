3ba6f5c1f7191399283afb3c81b9d52365fd1ff7
Hadas Zeilberger <hadaszeilberger@gmail.com>
2019-03-23 15:22:15 -0400

    working authorizer

--- packages/dont-crypto/app/dont-auth.hs
+++ packages/dont-crypto/app/dont-auth.hs
@@ -1,27 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
+import qualified Data.Text.Lazy as L
 import Network.AWS.Data
-import qualified Data.ByteString.Lazy as LBS
+import Data.ByteString.Lazy 
 import Network.AWS.Data.ByteString as ByteString
 import Data.Maybe
 import AWSLambda
 import AWSLambda.Events.APIGateway
 import Control.Lens
 import Data.Aeson as Aeson
+import Data.Aeson.Text
 import Data.Aeson.Embedded
 import Data.HashMap.Strict as HashMap
 import Data.Text 
 import AuthorizerIAMPolicy
 import qualified Amazonka.IAM.Policy as Policy
 
-authorizerMain 
-  :: (ToText authResponse)
-  => (Aeson.Value -> IO (APIGatewayProxyResponse authResponse))
-  -> IO ()
-authorizerMain = lambdaMain
 
-
-
-main = authorizerMain handler
+main = lambdaMain handler
 
 getString:: Aeson.Value -> Text
 getString (String string) = string
@@ -29,16 +24,14 @@ getString (String string) = string
 getHashMap:: Aeson.Value -> HashMap Text Aeson.Value
 getHashMap (Object hashMap) = hashMap
 
-getPropFromObj :: Aeson.Value -> Text -> Aeson.Value 
-getPropFromObj obj key = fromMaybe (String "") $  HashMap.lookup key (getHashMap obj)
+aesonObjectLookup :: Aeson.Value -> Text -> Aeson.Value 
+aesonObjectLookup obj key = fromMaybe (String "") $  HashMap.lookup key (getHashMap obj)
 
 
-handler :: Aeson.Value -> IO (APIGatewayProxyResponse ByteString)
+handler :: Aeson.Value -> IO (Aeson.Value)
 handler request = do
-  let requestContext= getPropFromObj request "requestContext"
-  let accountId = getString $ getPropFromObj requestContext "accountId"
-  let methodArn = getString $ getPropFromObj request "methodArn"
-  let policy =   fromMaybe HashMap.empty $ decode (encode (enableAccess accountId methodArn)) :: HashMap Text Value
-  let authorizer = insert "principalId" "boss" policy
-  pure $  responseOK & responseBody ?~ (LBS.toStrict $ encode (Object authorizer))
+  let requestContext= aesonObjectLookup request "requestContext"
+  let accountId = getString $ aesonObjectLookup requestContext "accountId"
+  let methodArn = getString $ aesonObjectLookup request "methodArn"  
+  pure $ Object $ HashMap.fromList [("principalId",String "boss"),("policyDocument",(toJSON $ enableAccess accountId methodArn))]
 
--- packages/dont-crypto/src/AuthorizerIAMPolicy.hs
+++ packages/dont-crypto/src/AuthorizerIAMPolicy.hs
@@ -8,8 +8,6 @@ import Control.Monad.Trans.AWS
 import Data.Text
 import Data.Maybe
 import qualified Amazonka.IAM.Policy as Policy
-constructARN :: Text -> Text -> Maybe Text-> Text -> Text -> Text
-constructARN accountId apiId stage method path= Data.Text.concat [(pack "arn:aws:execute-api:*:"), accountId, (pack ":"), apiId, (pack "/"), fromMaybe "" stage, method , path]
 
 theAction :: Policy.Block [Policy.Action]
 theAction =  Policy.some [Policy.Action (pack "execute-api:invoke")]
