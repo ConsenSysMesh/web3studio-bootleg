8199ba9c0f7ccf29e81e0cff86058445a4517ffd
Hadas Zeilberger <hadaszeilberger@gmail.com>
2019-04-10 18:19:39 -0400

    remove dont

--- packages/dont-crypto/cryptol/z3/include/z3_version.h
+++
@@ -1,7 +0,0 @@
-// automatically generated file.
-#define Z3_MAJOR_VERSION   4
-#define Z3_MINOR_VERSION   8
-#define Z3_BUILD_NUMBER    4
-#define Z3_REVISION_NUMBER 10272
-
-#define Z3_FULL_VERSION    "Z3 4.8.4.10272 d6df51951f4c master z3-4.8.4"
